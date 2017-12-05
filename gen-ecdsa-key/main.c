eee0f1c1d43de708504c348c7b6866b974cb011d
Ali Yousuf <aly.yousuf7@gmail.com>
2017-12-05 16:50:51 +0500

    Rewrite ECDSA Key Generation
    Signed-off-by: Ali Yousuf <aly.yousuf7@gmail.com>

--- gen-ecdsa-key/main.c
+++ gen-ecdsa-key/main.c
@@ -14,97 +14,103 @@ void initialize_fips(int mode) {
     }
 }
 
-int main() {
+int main(int argc, char* argv[]) {
     initialize_fips(1);
 
-    EC_KEY *key = EC_KEY_new_by_curve_name(NID_X9_62_prime256v1);
-    if (EC_KEY_generate_key(key) != 1) {
-        fprintf(stderr, "Failed to generate key");
-        return;
+    BIO               *outbio  = NULL;
+    EC_KEY            *myecc   = NULL;
+    EVP_PKEY          *pkey    = NULL;
+    int               eccgrp;
+    char*             key_type = "secp521r1";
+
+    /* ---------------------------------------------------------- *
+    * These function calls initialize openssl for correct work.  *
+    * ---------------------------------------------------------- */
+    OpenSSL_add_all_algorithms();
+    ERR_load_BIO_strings();
+    ERR_load_crypto_strings();
+
+    /* ---------------------------------------------------------- *
+    * Create the Input/Output BIO's.                             *
+    * ---------------------------------------------------------- */
+    outbio  = BIO_new(BIO_s_file());
+    outbio = BIO_new_fp(stdout, BIO_NOCLOSE);
+
+    /* ---------------------------------------------------------- *
+    * Read arguments.                                            *
+    * ---------------------------------------------------------- */
+    if (!(argc >= 2)) {
+        BIO_printf(outbio, "USAGE: %s [key_type]\n\n", argv[0]);
+    } else {
+        key_type = argv[1];
     }
 
-    // Public Key
-    BIGNUM *x = BN_new();
-    BIGNUM *y = BN_new();
-    BN_CTX *ctx = BN_CTX_new();
-    if (EC_POINT_get_affine_coordinates_GFp(
-        EC_KEY_get0_group(key),
-        EC_KEY_get0_public_key(key),
-        x, y, ctx
-    ) != 1) {
-        fprintf(stderr, "Public key error");
-        return;
-    }
-    BN_CTX_free(ctx);
-    
-    // Private key
-    BIGNUM *d = EC_KEY_get0_private_key(key);
-
-    // Print key
-    fprintf(stdout, "X: ");
-    do_bn_print(stdout, x);
-    fprintf(stdout, "\nY: ");
-    do_bn_print(stdout, y);
-    fprintf(stdout, "\nD: ");
-    do_bn_print(stdout, d);
-    fprintf(stdout, "\n");
-
-    // Some data for signing
-    unsigned char hash[] = "DnA68SOtgjCZRYq5NdjXf3RRPXKHHD44T3nDCilsC3CIQipZ1BblvKkLFyWofelxY36N8LDmewuqzg5fRdk6f4Khrbl3K5Mpgtqk";
-    
-    fprintf(stdout, "\nGenerating Signature\n");
-    fprintf(stdout, "\nHASH: %s", hash);
-  
-    ECDSA_SIG * signature = ECDSA_do_sign(hash, strlen(hash), key);
-  
-    if (signature == NULL) {
-        printf("Failed to generate EC Signature\n");
-        return NULL;
-    }   
-
-    if (verifySignature(hash, key, signature) != 1) {
-        return 0;
-    }
-    
-
-    BN_free(x);
-    BN_free(y);
-    EC_KEY_free(key);
-
-    return 1;
-}
-
-int do_bn_print(FILE *out, const BIGNUM *bn) {
-    int len, i;
-    unsigned char *tmp;
-    len = BN_num_bytes(bn);
-    if (len == 0)
-    {
-        fputs("00", out);
-        return 1;
+    /* ---------------------------------------------------------- *
+    * Create a EC key sructure, setting the group type from NID  *
+    * ---------------------------------------------------------- */
+    eccgrp = OBJ_txt2nid(key_type);
+    myecc = EC_KEY_new_by_curve_name(eccgrp);
+    if (!myecc) {
+        BIO_printf(outbio, "Invalid EC Curve.\n");
+        goto FreeAll;
     }
 
-    tmp = OPENSSL_malloc(len);
-    if (!tmp)
-    {
-        fprintf(stderr, "Memory allocation error\n");
-        return 0;
-    }
-    BN_bn2bin(bn, tmp);
-    for (i = 0; i < len; i++)
-        fprintf(out, "%02x", tmp[i]);
-    OPENSSL_free(tmp);
-    return 1;
-}
+    /* -------------------------------------------------------- *
+    * For cert signing, we use  the OPENSSL_EC_NAMED_CURVE flag*
+    * ---------------------------------------------------------*/
+    EC_KEY_set_asn1_flag(myecc, OPENSSL_EC_NAMED_CURVE);
 
-int verifySignature(unsigned char * hash, EC_KEY * eckey, ECDSA_SIG * signature) {
-    int verifyStatus = ECDSA_do_verify(hash, strlen(hash), signature, eckey);
-    
-    if (verifyStatus != 1) {
-      printf("\nFailed to verify EC Signature\n");
-      return 0;
+    /* -------------------------------------------------------- *
+    * Create the public/private EC key pair here               *
+    * ---------------------------------------------------------*/
+    if (! (EC_KEY_generate_key(myecc))) {
+        BIO_printf(outbio, "Error generating the ECC key.\n");
+        goto FreeAll;
     }
-    
-    printf("\nVerifed EC Signature\n");
-    return verifyStatus;
+
+    /* -------------------------------------------------------- *
+    * Converting the EC key into a PKEY structure let us       *
+    * handle the key just like any other key pair.             *
+    * ---------------------------------------------------------*/
+    pkey=EVP_PKEY_new();
+    if (!EVP_PKEY_assign_EC_KEY(pkey,myecc)) {
+        BIO_printf(outbio, "Error assigning ECC key to EVP_PKEY structure.\n");
+        goto FreeAll;
+    }
+
+    /* -------------------------------------------------------- *
+    * Now we show how to extract EC-specifics from the key     *
+    * ---------------------------------------------------------*/
+    myecc = EVP_PKEY_get1_EC_KEY(pkey);
+    const EC_GROUP *ecgrp = EC_KEY_get0_group(myecc);
+
+    /* ---------------------------------------------------------- *
+    * Here we print the key length, and extract the curve type.  *
+    * ---------------------------------------------------------- */
+    BIO_printf(outbio, "ECC Key size: %d bit\n", EVP_PKEY_bits(pkey));
+    BIO_printf(outbio, "ECC Key type: %s\n", OBJ_nid2sn(EC_GROUP_get_curve_name(ecgrp)));
+
+    /* ---------------------------------------------------------- *
+    * Here we print the private/public key data in PEM format.   *
+    * ---------------------------------------------------------- */
+    if(!PEM_write_bio_PrivateKey(outbio, pkey, NULL, NULL, 0, 0, NULL)) {
+        BIO_printf(outbio, "Error writing private key data in PEM format.\n");
+        goto FreeAll;
+    }
+
+    if(!PEM_write_bio_PUBKEY(outbio, pkey)) {
+        BIO_printf(outbio, "Error writing public key data in PEM format.\n");
+        goto FreeAll;
+    }
+
+    /* ---------------------------------------------------------- *
+    * Free up all structures                                     *
+    * ---------------------------------------------------------- */
+FreeAll:
+    EVP_PKEY_free(pkey);
+    EC_KEY_free(myecc);
+    BIO_free_all(outbio);
+
+    exit(0);
+    return 0;
 }
